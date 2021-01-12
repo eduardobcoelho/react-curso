e5787fd8c248c90c54ae315ec00dc1d8353c0ec3
eduardobcoelho <eduardobcoelho27@outlook.com>
2021-01-12 19:44:02 -0300

    revisao desafio repeticao

--- fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
@@ -2,24 +2,27 @@ import "./TabelaProdutos.css";
 import React from "react";
 import produtos from "../../data/produtos";
 
-export default (props) => {
+export default () => {
   const produtosList = produtos.map((produto, i) => {
     return (
-      <tr key={produto.id} className={i % 2 == 0 ? 'Par' : ''}>
+      <tr key={produto.id} className={i % 2 == 0 ? "Par" : ""}>
         <td>{produto.id}</td>
         <td>{produto.nomeProduto}</td>
-        <td>{'R$'}{produto.precoProduto}</td>
+        <td>
+          {"R$"}
+          {produto.precoProduto}
+        </td>
       </tr>
     );
   });
 
   return (
     <div className="TabelaProdutos">
-      <table border="1">
+      <table>
         <thead>
           <tr>
             <th>ID</th>
-            <th>Produto</th>
+            <th>Nome</th>
             <th>Preço</th>
           </tr>
         </thead>
